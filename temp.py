cd8e2b51aa86a7e007d1465bc8dd6111ed41a8aa
notshrirang <shrirangmahajan123@gmail.com>
2024-02-26 00:19:50 +0530

    feat: add temp file

--- temp.py
+++ temp.py
@@ -1,5 +1,10 @@
 # from easyocr.detection import get_detector, get_textbox
 import easyocr
+from PIL import Image
+import numpy as np
+
+image = Image.open('images/image.png')
+image = np.array(image)
 
 reader = easyocr.Reader(
     lang_list=["en"],
@@ -7,4 +12,4 @@ reader = easyocr.Reader(
 )
 # reader.get_detector, reader.get_textbox = get_detector, get_textbox
 reader.detector = 'models/best_accuracy.pth'
-print(reader.recognize('images/image.png'))
+print(reader.recognize(image, detail=0))
